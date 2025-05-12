7f3c09daa610f488044dbea2d1acbc91271bbfc4
csae8092 <p.andorfer@gmail.com>
2025-05-12 11:41:49 +0200

    feat: on hover on marker, highlight related text

--- html/js/airplane_detail_view_map.js
+++ html/js/airplane_detail_view_map.js
@@ -1,14 +1,3 @@
-// extract all unique places
-
-const spans = document.querySelectorAll('.dse-place[data-lng]');
-const places = Array.from(spans).map(span => {
-  const lat = span.dataset.lat;
-  const lng = span.dataset.lng;
-  const name = span.dataset.name;
-  const id = span.dataset.rsid;
-  return { lat, lng, name, id };
-});
-console.log(places);
 
 var map = L.map("airplaneMap").setView([48, 16], 6);
 
@@ -17,8 +6,30 @@ L.tileLayer("https://tile.openstreetmap.org/{z}/{x}/{y}.png", {
     '&copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors',
 }).addTo(map);
 
-places.forEach(place => {
-    L.marker([place.lat, place.lng])
-        .bindPopup(place.name)
-        .addTo(map);
+
+const spans = document.querySelectorAll(".dse-place[data-lng]");
+const places = Array.from(spans).map((span) => {
+  const lat = span.dataset.lat;
+  const lng = span.dataset.lng;
+  const name = span.dataset.name;
+  const id = span.dataset.rsid;
+  return { lat, lng, name, id };
+});
+
+places.forEach((place) => {
+  const marker = L.marker([place.lat, place.lng])
+    .bindPopup(place.name)
+    .addTo(map);
+  marker.id = place.id;
+
+  marker.on("mouseover", function (e) {
+    document.querySelectorAll(`[data-rsid="${e.target.id}"]`).forEach((el) => {
+      el.classList.add("highlight");
+    });
+  });
+  marker.on("mouseout", function (e) {
+    document.querySelectorAll(`[data-rsid="${e.target.id}"]`).forEach((el) => {
+      el.classList.remove("highlight");
+    });
+  });
 });
