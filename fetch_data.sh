c3b98783fb535aeb23fb7a487febfae21ca35ee6
csae8092 <p.andorfer@gmail.com>
2025-05-14 12:02:12 +0200

    less data processing on build

--- fetch_data.sh
+++ fetch_data.sh
@@ -9,9 +9,6 @@ mv ./daacda-data-main/data/ .
 
 rm main.zip
 rm -rf ./daacda-data-main
-add-attributes -g "./data/editions/*.xml" -b "https://daacda.acdh.oeaw.ac.at"
-add-attributes -g "./data/meta/*.xml" -b "https://daacda.acdh.oeaw.ac.at"
-add-attributes -g "./data/indices/*.xml" -b "https://daacda.acdh.oeaw.ac.at"
 
 echo "fetch imprint"
 ./shellscripts/dl_imprint.sh
