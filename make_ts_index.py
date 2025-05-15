854d0e88d3b5db212349fa7bc6e6d87e5c3300f2
csae8092 <p.andorfer@gmail.com>
2025-05-15 15:24:23 +0200

    wip: ts-index script done

--- make_ts_index.py
+++ make_ts_index.py
@@ -4,8 +4,6 @@ from acdh_tei_pyutils.tei import TeiReader
 from acdh_tei_pyutils.utils import (
     extract_fulltext,
     get_xmlid,
-    make_entity_label,
-    check_for_hash,
 )
 from acdh_xml_pyutils.xml import NSMAP
 from tqdm import tqdm
@@ -31,11 +29,11 @@ current_schema = {
         {"name": "rank", "type": "string", "facet": True, "sort": True},
         {"name": "role", "type": "string", "facet": True, "sort": True},
         {"name": "squadron", "type": "object", "facet": True},
-        # {"name": "bomb_group", "type": "string", "facet": True, "sort": True},
-        # {"name": "airforce", "type": "string", "facet": True, "sort": True},
-        # {"name": "prisons", "type": "object[]", "facet": True, "optional": True},
-        # {"name": "place_of_birth", "type": "object", "facet": True, "optional": True},
-        # {"name": "place_of_death", "type": "object", "facet": True, "optional": True},
+        {"name": "bomb_group", "type": "string", "facet": True, "sort": True},
+        {"name": "airforce", "type": "string", "facet": True, "sort": True},
+        {"name": "prisons", "type": "object[]", "facet": True, "optional": True},
+        {"name": "place_of_birth", "type": "string", "facet": True, "optional": True},
+        {"name": "place_of_death", "type": "string", "facet": True, "optional": True},
     ],
 }
 
@@ -48,22 +46,59 @@ for x in tqdm(items, total=len(items)):
     xml_id = get_xmlid(x)
     record["id"] = xml_id
     record["surname"] = extract_fulltext(x.xpath(".//tei:surname", namespaces=NSMAP)[0])
-    record["forename"] = extract_fulltext(x.xpath(".//tei:forename", namespaces=NSMAP)[0])
+    record["forename"] = extract_fulltext(
+        x.xpath(".//tei:forename", namespaces=NSMAP)[0]
+    )
     try:
-        middle_name = x.xpath(".//tei:forename[@type='middle']", namespaces=NSMAP)[0].text
+        middle_name = x.xpath(".//tei:forename[@type='middle']", namespaces=NSMAP)[
+            0
+        ].text
         record["full_name"] = f'{record["surname"]}, {record["forename"]} {middle_name}'
     except IndexError:
         record["full_name"] = f'{record["surname"]}, {record["forename"]}'
-    record["marc"] = extract_fulltext(x.xpath("./tei:idno[@type='marc-id']", namespaces=NSMAP)[0])
-    record["marc_destiny"] = extract_fulltext(x.xpath("./tei:note[@type='eintrag-macr']", namespaces=NSMAP)[0])
+    record["marc"] = extract_fulltext(
+        x.xpath("./tei:idno[@type='marc-id']", namespaces=NSMAP)[0]
+    )
+    record["marc_destiny"] = extract_fulltext(
+        x.xpath("./tei:note[@type='eintrag-macr']", namespaces=NSMAP)[0]
+    )
     record["rank"] = x.xpath("./tei:occupation/@role", namespaces=NSMAP)[0]
     record["role"] = extract_fulltext(x.xpath("./tei:occupation", namespaces=NSMAP)[0])
-    squad_id = x.xpath("./tei:affiliation[@type='squad']/tei:orgName/@key", namespaces=NSMAP)[0]
-    squad_label = extract_fulltext(x.xpath("./tei:affiliation[@type='squad']/tei:orgName", namespaces=NSMAP)[0])
-    record["squadron"] = {
-        "id": squad_id,
-        "label": squad_label
-    }
+    squad_id = x.xpath(
+        "./tei:affiliation[@type='squad']/tei:orgName/@key", namespaces=NSMAP
+    )[0]
+    squad_label = extract_fulltext(
+        x.xpath("./tei:affiliation[@type='squad']/tei:orgName", namespaces=NSMAP)[0]
+    )
+    record["squadron"] = {"id": squad_id, "label": squad_label}
+    record["bomb_group"] = extract_fulltext(
+        x.xpath("./tei:affiliation[@type='bomb-group']/tei:orgName", namespaces=NSMAP)[
+            0
+        ]
+    )
+    record["airforce"] = extract_fulltext(
+        x.xpath("./tei:affiliation[@type='airforce']/tei:orgName", namespaces=NSMAP)[0]
+    )
+    record["prisons"] = []
+    for y in x.xpath(".//tei:residence/tei:orgName", namespaces=NSMAP):
+        try:
+            item = {"id": y.attrib["key"], "label": extract_fulltext(y)}
+            record["prisons"].append(item)
+        except KeyError:
+            continue
+    try:
+        record["place_of_birth"] = extract_fulltext(
+            x.xpath(".//tei:birth/tei:placeName", namespaces=NSMAP)[0]
+        )
+    except IndexError:
+        pass
+    try:
+        record["place_of_death"] = extract_fulltext(
+            x.xpath(".//tei:death/tei:placeName", namespaces=NSMAP)[0]
+        )
+    except IndexError:
+        pass
+
     records.append(record)
 
 make_index = client.collections[COLLECTION_NAME].documents.import_(records)
